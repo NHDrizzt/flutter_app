a9e7515b90b353dbbc72047cbc606ee5af89cd2a
darthz <brulim.dtz@gmail.com>
2019-09-26 14:32:24 -0300

    Vamo la

--- lib/main.dart
+++ lib/main.dart
@@ -63,7 +63,69 @@ Card _cardFactory(int value, String txt) {
 }
 
 
+class MyApp extends StatelessWidget{
 
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title: 'TelaLogin',
+      theme: ThemeData(primarySwatch: Colors.orange),
+      home: LoginPage(),
+    );
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          TextFormField(
+            autofocus: true,
+            keyboardType: TextInputType.text,
+            style: new TextStyle(color: Colors.white, fontSize: 20),
+            decoration: InputDecoration(
+              labelText: "Login: ",
+              labelStyle: TextStyle(color: Colors.orange),
+            ),
+          ),
+          Divider(),
+          TextFormField(
+            autofocus: true,
+            obscureText: true,
+            keyboardType: TextInputType.text,
+            style: new TextStyle(color: Colors.white, fontSize: 20),
+            decoration: InputDecoration(
+              labelText: "Senha:",
+              labelStyle: TextStyle(color: Colors.orange),
+            ),
+          ),
+          Divider(),
+          ButtonTheme(
+            height: 60.0,
+            child: RaisedButton(
+              onPressed: () => {},
+              child: Text(
+                "Entrar",
+                style: TextStyle(color: Colors.white),
+              ),
+              color: Colors.orange,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
 
 
 
