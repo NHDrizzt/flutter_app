c39f3f03081940db53de2d1efdc8756b3efd5015
Gibs <gabrielop1ece@gmail.com>
2019-10-15 17:11:49 -0300

    Arrumei algumas coisas e deixei mais bonitinho.

--- lib/pages/Library/Andetails.dart
+++ lib/pages/Library/Andetails.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app/DAO/AnimeDAO.dart';
@@ -8,35 +10,11 @@ class detailsAnime extends StatefulWidget {
 }
 
 class _detailAn extends State<detailsAnime> {
-  @override
-  Widget build(BuildContext context) {
-    DocumentSnapshot doc = ModalRoute.of(context).settings.arguments;
-    return Scaffold(
-      body: _body(doc, context),
-      backgroundColor: Colors.orange,
-    );
-  }
-}
-
-Stack _body(DocumentSnapshot doc, BuildContext context) {
-  String imgname = doc['Imagem'];
-  //ImageController
-  final _img = Container(
-    child: ClipRRect(
-      borderRadius: new BorderRadius.only(
-        bottomRight: Radius.circular(0.0),
-        bottomLeft: Radius.circular(0.0),
-      ),
-      child: Hero(
-          tag: imgname,
-          child: Image.network(
-            imgname,
-            fit: BoxFit.cover,
-            height: 250,
-            width: 500,
-          )),
-    ),
-  );
+  double widthContainer = 300.0;
+  Icon icon = Icon(Icons.arrow_back_ios);
+  Color bckcolor = Colors.transparent;
+  Radius toprigthBorder = Radius.circular(5.0);
+  Radius botrigthBorder = Radius.circular(130.0);
 
   //AppBar
   final _posAppbar = AppBar(
@@ -54,30 +32,6 @@ Stack _body(DocumentSnapshot doc, BuildContext context) {
     elevation: 0.0,
   );
 
-  //Descrição do Anime
-  final _descAnime =
-  Positioned(
-    top: 300.0,
-    child: new Container(
-      margin: new EdgeInsets.symmetric(vertical: 45.0),
-      padding: EdgeInsets.all(15.0),
-      width: 350,
-      height: 500,
-      decoration: BoxDecoration(
-          color: Colors.orange,
-          borderRadius: BorderRadius.all(
-            Radius.circular(0.0),
-          )),
-      child:
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              _txtContent(doc['Descricao'], 16, FontWeight.w300)
-            ],
-          ),
-    ),
-  );
-
   //Transição de cores
   final _getGradient = Container(
     margin: new EdgeInsets.only(top: 170.0),
@@ -92,59 +46,153 @@ Stack _body(DocumentSnapshot doc, BuildContext context) {
     ),
   );
 
-  return Stack(
-    children: <Widget>[
-      _img,
-      _getGradient,
-      _posAppbar,
-      _descAnime,
-      _centerCardContainer(doc),
-    ],
+  @override
+  Widget build(BuildContext context) {
+    DocumentSnapshot doc = ModalRoute.of(context).settings.arguments;
+
+    final _icButtonCard = Container(
+      alignment: Alignment.topRight,
+      child:FloatingActionButton(
+         child: icon,
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          mini: true,
+          foregroundColor: Colors.black,
+          onPressed: (){
+            setState(() {
+              if (widthContainer == 15) {
+                botrigthBorder =  Radius.circular(140.0);
+                toprigthBorder =  Radius.circular(10.0);
+                widthContainer = 300;
+                icon = icon = Icon(Icons.arrow_back_ios);
+              } else {
+                botrigthBorder =  Radius.circular(200.0);
+                toprigthBorder =  Radius.circular(200.0);
+                icon = null;
+                widthContainer = 15;
+              }
+            });
+          })
+    );
+
+    //Card no meio da Tela
+    final _centerCard = Positioned(
+        top: 160.0,
+        child: GestureDetector(
+          onTap: () {
+            setState(() {
+              if (widthContainer == 15) {
+                botrigthBorder =  Radius.circular(140.0);
+                toprigthBorder =  Radius.circular(10.0);
+                widthContainer = 300;
+                icon = icon = Icon(Icons.arrow_back_ios);
+              } else {
+                botrigthBorder =  Radius.circular(200.0);
+                toprigthBorder =  Radius.circular(200.0);
+                widthContainer = 15;
+                icon = null;
+              }
+            });
+          },
+          child: new AnimatedContainer(
+            duration: Duration(milliseconds: 800),
+            //padding: EdgeInsets.all(8.0),
+            height: 180.0,
+            width: widthContainer,
+            child: Stack(
+              children: <Widget>[
+                _icButtonCard,
+                _centerCardContainer(doc),
+                ],
+            ),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  bottomRight: botrigthBorder,
+                  topRight: toprigthBorder
+                )),
+          ),
+        ));
+
+    return Scaffold(
+      //    body: _body(doc, context),
+      body: Stack(
+        children: <Widget>[
+          _img(doc, context),
+          // _getGradient,
+          _descAnime(doc),
+          _posAppbar,
+          _centerCard
+        ],
+      ),
+      backgroundColor: Colors.orange,
+    );
+  }
+}
+
+//Descrição do Anime
+Widget _descAnime(DocumentSnapshot doc) {
+  return Positioned(
+    top: 230.0,
+    child: new Container(
+        margin: new EdgeInsets.symmetric(vertical: 40.0),
+        padding: EdgeInsets.all(10.0),
+        width: 350,
+        height: 400,
+        child: ListView(
+          scrollDirection: Axis.vertical,
+          children: <Widget>[
+            _txtContent(doc['Descricao'], 17, FontWeight.w400),
+          ],
+        )),
   );
 }
 
-Row getRow(Container x, Container y, Container z) {
-  return Row(
-    mainAxisSize: MainAxisSize.max,
-    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-    children: <Widget>[x, y,z],
+//Image
+Widget _img(DocumentSnapshot doc, BuildContext context) {
+  String imgname = doc['Imagem'];
+  return Container(
+    child: ClipRRect(
+      borderRadius: new BorderRadius.only(
+        bottomRight: Radius.circular(2.0),
+        bottomLeft: Radius.circular(85.0),
+      ),
+      child: Hero(
+          tag: imgname,
+          child: Image.network(
+            imgname,
+            fit: BoxFit.cover,
+            height: 250,
+            width: 500,
+          )),
+    ),
   );
 }
 
 //conteúdo do card no meio da tela
 Widget _centerCardContainer(DocumentSnapshot doc) {
-  final _centerCardContent = Column(
-    crossAxisAlignment: CrossAxisAlignment.start,
-    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+  final _centerCardContent = ListView(
+    scrollDirection: Axis.horizontal,
+    padding:EdgeInsets.all(15.0) ,
     children: <Widget>[
-      _txtContent(doc['Nome'], 25.0, FontWeight.w700),
-      getRow(
-        _TileContent('Categoria', doc['Categoria']),
-        _TileContent('Duração', doc['Duracao']),
-        _TileContent('Estúdio', doc['Estudio']),
-      ),
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          _txtContent(doc['Nome'], 25.0, FontWeight.w700),
+          getRow(
+            _TileContent('Categoria', doc['Categoria']),
+            _TileContent('Duração', doc['Duracao']),
+            _TileContent('Estúdio', doc['Estudio']),
+          ),
+        ],
+      )
     ],
   );
-
-//Card no meio da Tela
-  final _centerCard = Positioned(
-    top: 165.0,
-    child: new Container(
-      margin: new EdgeInsets.symmetric(horizontal: 20.0),
-      padding: EdgeInsets.all(20.0),
-      height: 180.0,
-      width: 300.0,
-      child: _centerCardContent,
-      decoration: BoxDecoration(
-          color: Colors.white70,
-          borderRadius: BorderRadius.all(
-            Radius.circular(15.0),
-          )),
-    ),
-  );
-  return _centerCard;
+  return _centerCardContent;
 }
 
+//Utilitários
 Text _txtContent(String type, double fontsize, FontWeight fw) {
   return Text(
     type,
@@ -167,5 +215,13 @@ Container _TileContent(String title, String subtitles) {
         _txtContent(subtitles, 12, FontWeight.w400),
       ],
     ),
+  );
+}
+
+Row getRow(Container x, Container y, Container z) {
+  return Row(
+    mainAxisSize: MainAxisSize.max,
+    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    children: <Widget>[x, y, z],
   );
 }
