5d6ed3626f354c2ecf19346e396e4aefc50940e8
Gabriel de Sousa <biel_s.carvalho2@hotmail.com>
2019-11-07 15:59:52 -0300

    Drawer+MinhaBiblioteca+LogicaDeRotas.

---
+++ lib/pages/PerfilGui.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/pages/Perfil.dart';
+import 'PageState.dart';
+import 'Perfil.dart';
+
+class Guillotine extends StatefulWidget {
+  @override
+  _GuillotineState createState() => _GuillotineState();
+}
+
+class _GuillotineState extends State<Guillotine> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      top: false,
+      bottom: false,
+      child: new Container(
+        child: new Stack(
+          alignment: Alignment.topLeft,
+          children: <Widget>[
+            new Page(),
+            new Perfil(),
+          ],
+        ),
+      ),
+    );
+  }
+}
